a72b8552351eaf5a5448c1664c540c86a51de1cc
maroofkhan <khan.maroof6569@gmail.com>
2022-04-08 01:22:18 -0500

    nearfinal

---
+++ Connect4.md
@@ -0,0 +1,17 @@
+                                                                                            Connect 4 
+
+1. Finding out what constants I need for color.
+2. Find out what variables that are needed for the Board.
+3. Put an element for which player turn it is.
+4. Add event listeners for buttons such as rest or play again. 
+5. Use function for gameBoard and randomize which player it is.
+6. Initializing the board and find out which person will win when they have a connect 4. 
+have render function to render the button and the board.
+
+
+From the start I am going to work on the div to make the board which will equal to 42 dives and that will be 7 rows and 6 columns after the html is setup 
+start on the css which will make everything come to life. Using css define the borders, colors, and the With, hight for the game. Start working on the js 
+which will make the game work. First define all your const to start off then start adding your element selector. To start off you want to get all the basic 
+done before you start working on your win logic. Start making your board inside an array. Then define the elements for your buttons. After your get the basic 
+done you want to start off by wring down how you want to work on the win logic, First you can work on the vertical and horizontal which will lead you to work on
+the diagonal win logic. 
--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 /*----- constants -----*/
 const COLORS = {
     '0': 'white',
-    '1': 'purple',
-    '-1': 'orange'
+    '1': 'Red',
+    '-1': 'Blue'
   };
   
   /*----- app's state (variables) -----*/
@@ -17,8 +17,6 @@ const markerEls = [...document.querySelectorAll('#markers > div')];
 const turnMsg = document.querySelector('h2');
 const message = document.getElementById('message')
 const resetBtn = document.getElementById('reset')
-
-// const winnerMsg = document.querySelector('')
   
   /*----- event listeners -----*/
 document.getElementById('playAgain').addEventListener('click', init);
@@ -26,8 +24,7 @@ document.getElementById('markers').addEventListener('click', handleDrop);
 resetBtn.addEventListener('click', init);
   /*----- functions -----*/
 init();
-  
-  // initialize state, then call render()
+
 function init() {
     board = [
       [0, 0, 0, 0, 0, 0], 
@@ -48,13 +45,12 @@ function init() {
   function checkVertWin(colIdx, rowIdx) {
     const player = board[colIdx][rowIdx];
     let count = 1; 
-    //count up
-    let idx = rowIdx + 1; // initialize to one above 
+    let idx = rowIdx + 1; 
     while (idx < board[idx].length && board[colIdx][idx] === player) {
       count++;
       idx++;
     }
-    idx = rowIdx - 1; // initialize to one above 
+    idx = rowIdx - 1; 
     while (idx >= 0 && board[colIdx][idx] === player) {
       count++;
       idx--;
@@ -62,18 +58,15 @@ function init() {
     return count === 4 ? renderWinner(player): null; 
   }
   
-  
-  //opposite
   function checkHorzWin(colIdx, rowIdx) {
     const player = board[colIdx][rowIdx];
     let count = 1; 
-    //count right
-    let idx = colIdx + 1; // initialize to one above 
+    let idx = colIdx + 1;
     while (idx < board.length && board[idx][rowIdx] === player) {
       count++;
       idx++;
     }
-    idx = colIdx - 1; // initialize to one above 
+    idx = colIdx - 1; 
     while (idx >= 0 && board[idx][rowIdx] === player) {
       count++;
       idx--;
@@ -81,7 +74,6 @@ function init() {
     return count >= 4 ? renderWinner(player): null;
   }
 function render() {
-    // Iterate over the column arrays
     board.forEach(function(colArr, colIdx) { 
         console.log(colArr);
           colArr.forEach(function(cellVal, rowIdx) {
@@ -93,10 +85,9 @@ function render() {
     });
     renderMarkers();
 
-    turnMsg.innerText = `${COLORS[turn]}Turn`;
+    turnMsg.innerText = `${COLORS[turn]}`;
   }
   
-  // hide/show the markers (hide if no 0's exist in that column)
   function renderMarkers() {
     markerEls.forEach(function(markerEl, colIdx) {
       markerEl.style.visibility = board[colIdx].includes(0) ? 'visible' : 'hidden';
@@ -109,7 +100,6 @@ function render() {
     winner = true;
   }
 
-  // Update all impacted state, then call render
   function handleDrop(evt) {
     if (winner) return;
     const colIdx = markerEls.indexOf(evt.target);
@@ -137,21 +127,6 @@ function render() {
         message.innerHTML = 'its a tie'
     };
   }
-  // function checkWin() {
-  //   for(let i=0; i < board.length -4; i++){
-  //     for(let j=0; j < board[i].length -4; j++){
-  //       if(board[i] [j]=== turn && board[i][j+1] === turn && board[i][j+2] && board[i][j+3]){
-  //         winner = true
-  //       } else if(board[i][j]=== turn && board[i+1][j] === turn && board[i+2][j] === turn && board[i+3][j]){
-  //         winner = true
-  //       }else if(board[i][j]=== turn && board[i+1][j+1] === turn && board[i+2][j+2] === turn && board[i+3][j+3]){
-  //         winner = true
-  //       }else if(board[i][j]=== turn && board[i+1][j-1] === turn && board[i+2][j-2] === turn && board[i+3][j-3]){
-  //         winner = true
-  //     }
-  //   }
-  // }
-  // };
 
 
 
